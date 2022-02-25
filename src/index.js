55528ee055afaca00b0a965774078086709a4fc6
rodrigo <rjara001@gmail.com>
2022-02-25 19:15:40 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const App = () => {
+    return <h1>This is a great application from scratch</h1>
+}
+
+ReactDOM.render(<App></App>, document.getElementById('app'));
